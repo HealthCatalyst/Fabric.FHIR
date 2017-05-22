487f043eceac21d587db8ae448b0fd0deefa1f7a
HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>
2017-05-22 11:05:18 -0700

    added a dummy Patient module
    Signed-off-by: HQCATALYST\imran.qureshi <imran.qureshi@healthcatalyst.com>

---
+++ Fabric.FHIR/Modules/PatientModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Nancy;
+
+namespace Fabric.FHIR.Modules
+{
+    public class PatientModule : NancyModule
+    {
+        public PatientModule()
+        {
+            Get("/Patient/{id}", parameters =>
+            {
+                return $"I am a Patient: {parameters.id}";
+            });
+        }
+    }
+}
